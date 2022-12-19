8035691d60e1896f2d49d301059a2e7e4906db42
Samuel <samuelmachado1@gmail.com>
2022-12-19 16:27:53 -0300

    [developer]Testando renderizações condicionais e atualizando css

--- memories/src/app/app.module.ts
+++ memories/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentComponentComponent } from './components/parent-component/parent-component.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ParentComponentComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule,
