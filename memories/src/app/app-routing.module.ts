363e8dbd2cd4dd3c4e4315e4b98570fe308fd444
Samuel <samuelmachado1@gmail.com>
2022-12-19 19:00:58 -0300

    [developer]Testando rotas e requisições http. UTilizando json-server

--- memories/src/app/app-routing.module.ts
+++ memories/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ParentComponentComponent } from './components/parent-component/parent-component.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'page1', component: ParentComponentComponent },
+  { path: 'page2', component: DirectivesComponent },
+  { path: 'page3', component: IfRenderComponent },
+  { path: 'page4', component: EventosComponent },
+  { path: 'page5', component: EmitterComponent },
+  { path: 'page6', component: PipesComponent },
+  { path: 'page7', component: TwoWayBindingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- memories/src/app/components/list-render/list-render.component.ts
+++ memories/src/app/components/list-render/list-render.component.ts
@@ -1,24 +1,32 @@
 import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
+import { ListService } from 'src/app/services/list.service';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals: Animal[] = [
-    { name: 'pet', type: 'Dog', age: 2 },
-    { name: 'pet1', type: 'Cat', age: 10 },
-    { name: 'pet2', type: 'Fish', age: 4 },
-    { name: 'pet3', type: 'Dog', age: 3 },
-    { name: 'pet4', type: 'Cat', age: 6 },
-  ];
+  constructor(private listService: ListService) {
+    this.getAnimals();
+  }
+
+  animals: Animal[] = [];
 
   animalDetails = '';
 
   showAge(animal: Animal) {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
+  }
+
+  removeAnimal(animal: Animal) {
+    console.log("Removendo animal...")
+    this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
 
 }
