4e6a66f713e6853f49a6831a43419eae93da19a8
Samuel <samuelmachado1@gmail.com>
2022-12-19 17:38:19 -0300

    [developer]Testando implementação de interfaces

--- memories/src/app/components/list-render/list-render.component.ts
+++ memories/src/app/components/list-render/list-render.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals = [
-    { name: 'pet', type: 'Dog' },
-    { name: 'pet1', type: 'Cat' },
-    { name: 'pet2', type: 'Fish' },
-    { name: 'pet3', type: 'Dog' },
-    { name: 'pet4', type: 'Cat' },
+  animals: Animal[] = [
+    { name: 'pet', type: 'Dog', age: 2 },
+    { name: 'pet1', type: 'Cat', age: 10 },
+    { name: 'pet2', type: 'Fish', age: 4 },
+    { name: 'pet3', type: 'Dog', age: 3 },
+    { name: 'pet4', type: 'Cat', age: 6 },
   ];
+
+  animalDetails = '';
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
+  }
 
 }
